5879c5e1009b7d19557c1c52afae69fbecf6a938
Mohammad Saad Ahmad <msaadahmad421@gmail.com>
2024-02-01 01:19:48 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
 let form = document.querySelector('form');
+let firstnameInput = document.querySelector('#first-box');
+let lastnameInput = document.querySelector('#last-box');
 let usernameInput = document.querySelector('#user-box');
 let passwordInput = document.querySelector('#pass-box');
-let signBtn = document.querySelector('#sign-btn');
+let cnfrm_passwordInput = document.querySelector('#confrm-box');
+let signBtn = document.querySelector('#regis-btn');
 
 
 form.addEventListener('submit', (event) => {
   
   event.preventDefault();
 
-  if (usernameInput.value === '' || passwordInput.value === '') {
+  if (firstnameInput.value === '' || passwordInput.value === '' || cnfrm_passwordInput.value === '') {
     
-    alert('One or more details are incorrect/missing.');
+    alert('Please enter all the details!');
+  } else if (passwordInput.value !== cnfrm_passwordInput.value) {
+
+    alert('Your passwords do not match!');
   } else {
 
     form.submit();
-    alert('The form has been successfully submitted!');
+    alert('The registration has been successful!');
 
     setTimeout(() => {
         form.reset();
       }, 5);
     }
+});
+
+cnfrm_passwordInput.addEventListener('input', () => {
+  if (passwordInput.value !== cnfrm_passwordInput.value) {
+    cnfrm_passwordInput.setCustomValidity('Passwords do not match');
+  } else {
+    cnfrm_passwordInput.setCustomValidity('');
+  }
 });
